c32c9b66be60cfe9ca65bd55b4b55436d7e269b5
sardorkobilzonov <sardorkobilzonov4@gmail.com>
2024-11-05 16:22:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+//1-masala
+// {
+//   let n = 4,
+//     k = 35;
+//   for (let i = 0; i < n; i++) {
+//     console.log(k);
+//   }
+// }
+
+//2-masala
+
+// {
+//   let a = 1,
+//     b = 10;
+//   for (let i = a; i <= b; i++) {
+//     console.log(i);
+//   }
+// }
+
+//3-masala
+// {
+//   let a = 4;
+//   let b = 18;
+//   let count = 0;
+//   for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+//   }
+//   for (let i = b - 1; i > a; i--) {
+//     count++;
+//   }
+//   console.log(`count ${count}`);
+// }
+
+//4-masala
+// {
+//   let sweetPrise = 2;
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(sweetPrise * i);
+//   }
+// }
+
+//5-masala
+
+// {
+//   let sweetPrise = 16,
+//     count = 0.5,
+//     sum = 0;
+//   for (let i = 0; i <= count; i += 0.1) {
+//     sum = i * sweetPrise;
+//   }
+//   console.log(sum.toFixed(2));
+// }
+// {
+//   let sweetPrise = 2;
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(sweetPrise * i);
+//   }
+// }
+
+//6-masala
+// {
+//   let sweetPrise = 16,
+//     count = 1.2,
+//     sum = 0;
+//   for (let i = 0; i <= count; i += 0.2) {
+//     sum = i * sweetPrise;
+//   }
+//   console.log(sum.toFixed(2));
+// }
+
+//7-masala
+// {
+//   let a = 5,
+//     b = 10,
+//     sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     sum += i;
+//   }
+//   console.log(sum);
+// }
+
+//8-masala
+// {
+//   let a = 5,
+//     b = 10,
+//     sum = 1;
+//   for (let i = a; i <= b; i++) {
+//     sum *= i;
+//   }
+//   console.log(sum);
+// }
+
+//9-masala
+// {
+//   let a = 5,
+//     b = 6,
+//     sum = 0;
+//   for (let i = a; i <= b; i++) {
+//     sum += i ** 2;
+//   }
+//   console.log(sum);
+// }
+
+//10-masala
+// {
+//   let n = 7,
+//     s = 0;
+//   for (let i = 1; i <= n; i++) {
+//     s += 1 / i;
+//   }
+//   console.log(s);
+// }
